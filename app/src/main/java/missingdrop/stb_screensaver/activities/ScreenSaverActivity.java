98dd6dbd0f8e7f2f9f1e1e1482a47d379fe87ff5
aaghan <sadip_aacharya@hotmail.com>
2016-03-28 10:58:12 +0545

    kills all background process from pid as well package name

--- app/src/main/java/missingdrop/stb_screensaver/activities/ScreenSaverActivity.java
+++ app/src/main/java/missingdrop/stb_screensaver/activities/ScreenSaverActivity.java
@@ -1,14 +1,22 @@
 package missingdrop.stb_screensaver.activities;
 
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningAppProcessInfo;
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.koushikdutta.ion.Ion;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import missingdrop.stb_screensaver.MyApplication;
@@ -20,24 +28,98 @@ import missingdrop.stb_screensaver.R;
  */
 public class ScreenSaverActivity extends Activity {
     ImageView imgScreenSaver;
-    LinearLayout screenSaverLayout;
+    FrameLayout screenSaverLayout;
     Handler screenSaverHandler;
     Runnable screenSaverRunnable;
+    TextView txtTime;
     MyApplication serUtil;
-    public MyApplication getApp(){
-        return (MyApplication)this.getApplication();
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen_saver);
-        screenSaverLayout = (LinearLayout) findViewById(R.id.layout_screen_saver);
+        Toast.makeText(this, "ScreenSaverActivity", Toast.LENGTH_SHORT).show();
+        getApp().setScreenSaverStarted(true);
+        screenSaverLayout = (FrameLayout) findViewById(R.id.layout_screen_saver);
         imgScreenSaver = (ImageView) findViewById(R.id.img_screenSaver);
-//        Bundle bundle = getIntent().getExtras();
+        txtTime = (TextView) findViewById(R.id.text_time);
+        Bundle bundle = getIntent().getExtras();
         imgScreenSaver.setImageResource(R.drawable.screensaver);
+        changePositionForTextView();
 //        repeatScreenSaver(bundle.getStringArrayList("imageList"), bundle.getInt("delay"));
 //        repeatScreenSaver("",bundle.getInt("delay"));
+        killBackgroundApps();
+    }
+
+    public MyApplication getApp() {
+        return (MyApplication) this.getApplication();
+    }
+
+    private void changePositionForTextView() {
+        final FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(txtTime.getLayoutParams());
+        final Random random = new Random();
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (getApp().getScreenSaverStarted()) {
+                    try {
+                        Thread.sleep(8000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    params.setMargins(random.nextInt(500), random.nextInt(500),
+                                      random.nextInt(200), random.nextInt(200));
+                    runOnUiThread(new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            txtTime.setLayoutParams(params);
+                            Log.d(".ScreenSaverActivity", "layoutParams new set");
+                        }
+                    }));
+
+
+                }
+
+            }
+        });
+
+        t.start();
+
+    }
+
+    private void killBackgroundApps() {
+        ActivityManager am = (ActivityManager) this.getSystemService(Context.ACTIVITY_SERVICE);
+        List<RunningAppProcessInfo> tasks = am.getRunningAppProcesses();
+        //for loop to kill all process except currentApp
+        for (ActivityManager.RunningAppProcessInfo process : tasks) {
+            if (process.pid != tasks.get(0).pid) {
+                android.os.Process.killProcess(process.pid);
+            }
+        }
+        for (ApplicationInfo packageInfo : getPackageManager().getInstalledApplications(0)) {
+            if (packageInfo.packageName != getPackageName()) {
+                am.killBackgroundProcesses(packageInfo.packageName);
+            }
+
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        getApp().setScreenSaverStarted(false);
+        finish();
+    }
+
+    @Override
+    public void onUserInteraction() {
+        super.onUserInteraction();
+        getApp().active();
+
+//        ((MyApplication) this.getApplicationContext()).setUserInteractionThread(8000);
+//        ((MyApplication) this.getApplicationContext()).startThread();
+
+
     }
 
     private void repeatScreenSaver(final ArrayList<String> imageList, final int milliseconds) {
@@ -46,42 +128,24 @@ public class ScreenSaverActivity extends Activity {
         screenSaverRunnable = new Runnable() {
             @Override
             public void run() {
-//                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(imgScreenSaver.getLayoutParams());
-//                Random random = new Random();
-//
-//                params.setMargins(random.nextInt(500), random.nextInt(500),
-//                        random.nextInt(200), random.nextInt(200));
-//                imgScreenSaver.setLayoutParams(params);
-                Ion.with(ScreenSaverActivity.this)
-                        .load(imageList.get(
-                                        new Random().nextInt(imageList.size()
-                                        )
-                                )
-                        )
-                        .withBitmap()
-                        .error(R.drawable.screensaver)
-                        .intoImageView(imgScreenSaver);
-                imgScreenSaver.setImageResource(R.drawable.screensaver);
+
+                if (imageList.size() > 1)
+                    Ion.with(ScreenSaverActivity.this)
+                            .load(imageList.get(
+                                          new Random().nextInt(imageList.size()
+                                          )
+                                  )
+                            )
+                            .withBitmap()
+                            .error(R.drawable.screensaver)
+                            .intoImageView(imgScreenSaver);
+                else
+                    imgScreenSaver.setImageResource(R.drawable.screensaver);
                 screenSaverHandler.postDelayed(this, milliseconds);
+
 
             }
         };
         screenSaverHandler.postDelayed(screenSaverRunnable, milliseconds);
     }
-
-
-    @Override
-    public void onUserInteraction() {
-        super.onUserInteraction();
-        getApp().active();
-        getApp().setScreenSaverStarted(false);
-        finish();
-//        ((MyApplication) this.getApplicationContext()).setUserInteractionThread(8000);
-//        ((MyApplication) this.getApplicationContext()).startThread();
-
-
-
-    }
-
-
 }
